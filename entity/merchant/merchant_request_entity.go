e68963b9dd40774f38718342964ddf15e7fac842
vinutama <muhammadkhevin@gmail.com>
2024-05-27 02:48:11 +0700

    add some validator and remove unused validator

--- entity/merchant/merchant_request_entity.go
+++ entity/merchant/merchant_request_entity.go
@@ -2,12 +2,12 @@ package merchant_entity
 
 type AddMerchantRequest struct {
 	Name     string           `json:"name" validate:"required,min=2,max=30"`
-	Category string           `json:"merchantCategory" validate:"required,merchantCategory"`
+	Category string           `json:"merchantCategory" validate:"required,validateCategory=merchant"`
 	ImageUrl string           `json:"imageUrl" validate:"required,validateUrl"`
 	Location *LocationDetails `json:"location" validate:"required"`
 }
 
 type LocationDetails struct {
-	Latitude  float64 `json:"latitude" validate:"required,latitudeValidation"`
-	Longitude float64 `json:"longitude" validate:"required,longitudeValidation"`
+	Latitude  float64 `json:"latitude" validate:"required,validateGeoCoord=lat"`
+	Longitude float64 `json:"longitude" validate:"required,validateGeoCoord=long"`
 }
