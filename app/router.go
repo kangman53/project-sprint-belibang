ffb074d17a322165f40cab2edc9f966d18a1221b
vinutama <muhammadkhevin@gmail.com>
2024-05-27 02:27:27 +0700

    add router URL for POST merchant

--- app/router.go
+++ app/router.go
@@ -4,9 +4,11 @@ import (
 	"github.com/kangman53/project-sprint-belibang/controller"
 	"github.com/kangman53/project-sprint-belibang/helpers"
 
+	merchant_repository "github.com/kangman53/project-sprint-belibang/repository/merchant"
 	user_repository "github.com/kangman53/project-sprint-belibang/repository/user"
 	auth_service "github.com/kangman53/project-sprint-belibang/service/auth"
 	file_service "github.com/kangman53/project-sprint-belibang/service/file"
+	merchant_service "github.com/kangman53/project-sprint-belibang/service/merchant"
 	user_service "github.com/kangman53/project-sprint-belibang/service/user"
 
 	"github.com/go-playground/validator"
@@ -28,6 +30,10 @@ func RegisterBluePrint(app *fiber.App, dbPool *pgxpool.Pool) {
 	fileService := file_service.NewFileService()
 	fileController := controller.NewFileController(fileService)
 
+	merchantRepository := merchant_repository.NewMerchantRepository(dbPool)
+	merchantService := merchant_service.NewMerchantService(merchantRepository, validator)
+	merchantController := controller.NewMerchantController(merchantService)
+
 	// Users API
 	adminApi := app.Group("/admin")
 	adminApi.Post("/register", userController.Register)
@@ -40,4 +46,8 @@ func RegisterBluePrint(app *fiber.App, dbPool *pgxpool.Pool) {
 	// app.Use(helpers.CheckTokenHeader)
 	app.Use(helpers.GetTokenHandler())
 	app.Post("/image", authService.AuthorizeRole("admin"), fileController.Upload)
+
+	// Merchants API
+	merchantApi := adminApi.Group("/merchants")
+	merchantApi.Post("/", authService.AuthorizeRole("admin"), merchantController.Add)
 }
