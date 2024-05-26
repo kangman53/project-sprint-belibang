d856723d762c4da597e29b788ac72daf9f1be58c
vinutama <muhammadkhevin@gmail.com>
2024-05-27 03:13:03 +0700

    add item entities

---
+++ entity/item/item_request_entity.go
@@ -0,0 +1,8 @@
+package item_entity
+
+type AddItemRequest struct {
+	Name     string `json:"name" validate:"required,min=2,max=30"`
+	Category string `json:"productCategory" validate:"required,validateCategory=item"`
+	Price    int    `json:"price" validate:"required,min=1"`
+	ImageUrl string `json:"imageUrl" validate:"required,validateUrl"`
+}
---
+++ entity/item/item_response_entity.go
@@ -0,0 +1,5 @@
+package item_entity
+
+type AddItemResponse struct {
+	Id string `json:"itemId"`
+}
