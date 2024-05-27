cc3cb95ff698d35a8042573bc6435d8dc1d1f951
vinutama <muhammadkhevin@gmail.com>
2024-05-28 00:29:52 +0700

    add merchants repos

--- repository/merchant/merchant_repository.go
+++ repository/merchant/merchant_repository.go
@@ -8,4 +8,5 @@ import (
 
 type MerchantRepository interface {
 	Add(ctx context.Context, req merchant_entity.Merchant) (string, error)
+	Search(ctx context.Context, query merchant_entity.SearchMerchantQuery) (*[]merchant_entity.SearchMerchantData, error)
 }
