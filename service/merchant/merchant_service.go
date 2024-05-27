eb630fdd799add97e17d6867f766a9b34fa2db9a
vinutama <muhammadkhevin@gmail.com>
2024-05-28 00:34:50 +0700

    add merchants service get

--- service/merchant/merchant_service.go
+++ service/merchant/merchant_service.go
@@ -1,10 +1,13 @@
 package merchant_service
 
 import (
+	"context"
+
 	"github.com/gofiber/fiber/v2"
 	merchant_entity "github.com/kangman53/project-sprint-belibang/entity/merchant"
 )
 
 type MerchantService interface {
 	Add(ctx *fiber.Ctx, req merchant_entity.AddMerchantRequest) (merchant_entity.AddMerchantResponse, error)
+	Search(ctx context.Context, req merchant_entity.SearchMerchantQuery) (merchant_entity.SearchMerchantResponse, error)
 }
