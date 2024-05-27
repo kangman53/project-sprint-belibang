9bc42d048e69d6880f7d106d46648ae1a0a5d888
Rizqi Taufiq <rizqitaufiqf@gmail.com>
2024-05-27 13:00:19 +0700

    add search nearby repository

--- repository/merchant/merchant_repository.go
+++ repository/merchant/merchant_repository.go
@@ -8,4 +8,5 @@ import (
 
 type MerchantRepository interface {
 	Add(ctx context.Context, req merchant_entity.Merchant) (string, error)
+	SearchNearby(ctx context.Context, query merchant_entity.SearchNearbyMerchantQuery, latitude float64, longitude float64) (*[]merchant_entity.SearchNearbyMerchantData, error)
 }
