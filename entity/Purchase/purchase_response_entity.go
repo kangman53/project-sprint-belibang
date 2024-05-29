0dfaf7ef9dee711709b2aee1e91f6579d997a430
vinutama <muhammadkhevin@gmail.com>
2024-05-29 22:49:27 +0700

    add search order entities

--- entity/Purchase/purchase_response_entity.go
+++ entity/Purchase/purchase_response_entity.go
@@ -1,5 +1,10 @@
 package purchase_entity
 
+import (
+	item_entity "github.com/kangman53/project-sprint-belibang/entity/item"
+	merchant_entity "github.com/kangman53/project-sprint-belibang/entity/merchant"
+)
+
 type PurchaseEstimateResponse struct {
 	TotalPrice                     int    `json:"totalPrice"`
 	EstimatedDeliveryTimeInMinutes int    `json:"estimatedDeliveryTimeInMinutes"`
@@ -8,4 +13,14 @@ type PurchaseEstimateResponse struct {
 
 type PurchaseOrderResponse struct {
 	OrderId string `json:"orderId"`
+}
+
+type SearchHistoryOrderResponse struct {
+	OrderId string                      `json:"orderId"`
+	Orders  *[]SearchHistoryOrderOrders `json:"orders"`
+}
+
+type SearchHistoryOrderOrders struct {
+	Merchant *merchant_entity.MerchantData `json:"merchant"`
+	Items    *[]item_entity.SearchItemData `json:"items"`
 }
